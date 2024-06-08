ea5badeb96e069b08d2bb85c77b586abdf887990
ReikiPD <reikiputradarmawan@gmail.com>
2024-06-09 01:14:44 +0700

    reiki

--- frontend/src/components/FixtureDetailPage.jsx
+++ frontend/src/components/FixtureDetailPage.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
-import '../style/FixtureDetailPage.css'; // Import CSS file for styling
-import { PageContent } from '../Header'; // Import the PageContent component
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import "../style/FixtureDetailPage.css"; // Import CSS file for styling
+import { PageContent } from "../Header"; // Import the PageContent component
 
 const FixtureDetailPage = () => {
   const { fixture_id } = useParams();
@@ -12,19 +12,23 @@ const FixtureDetailPage = () => {
   useEffect(() => {
     const fetchListedPlayers = async () => {
       try {
-        const response = await axios.get(`http://localhost:5000/api/players/listed_players/${fixture_id}`);
+        const response = await axios.get(
+          `http://localhost:5000/api/players/listed_players/${fixture_id}`
+        );
         setListedPlayers(response.data);
       } catch (error) {
-        console.error('Error fetching listed players:', error);
+        console.error("Error fetching listed players:", error);
       }
     };
 
     const fetchPlayerStats = async () => {
       try {
-        const response = await axios.get(`http://localhost:5000/api/playerStats/fixture/${fixture_id}`);
+        const response = await axios.get(
+          `http://localhost:5000/api/playerStats/fixture/${fixture_id}`
+        );
         setPlayerStats(response.data.data);
       } catch (error) {
-        console.error('Error fetching player stats:', error);
+        console.error("Error fetching player stats:", error);
       }
     };
 
@@ -34,53 +38,56 @@ const FixtureDetailPage = () => {
 
   return (
     <PageContent>
-        <div>
+      <div>
         <h2>Listed Players</h2>
         <table className="stats-table">
-            <thead>
+          <thead>
             <tr>
-                <th>Player Name</th>
-                <th>Club Name</th>
+              <th>Player Name</th>
+              <th>Club Name</th>
             </tr>
-            </thead>
-            <tbody>
+          </thead>
+          <tbody>
             {listedPlayers.map((player) => (
-                <tr key={player.id}>
+              <tr key={player.id}>
                 <td>{player.player_name}</td>
                 <td>{player.club_name}</td>
-                </tr>
+              </tr>
             ))}
-            </tbody>
+          </tbody>
         </table>
-        
+
         <h2>Player Stats</h2>
         <table className="stats-table">
-            <thead>
+          <thead>
             <tr>
-                <th>Player Name</th>
-                <th>Goals</th>
-                <th>Shots</th>
-                <th>Shots on Target</th>
-                <th>Passes</th>
-                {/* Add more table headers for other stats */}
+              <th>Player Name</th>
+              <th>Goals</th>
+              <th>Shots</th>
+              <th>Shots on Target</th>
+              <th>Passes</th>
+              <th>Assists</th>
+              <th>Tackles</th>
+              {/* Add more table headers for other stats */}
             </tr>
-            </thead>
-            <tbody>
+          </thead>
+          <tbody>
             {playerStats.map((stat) => (
-                <tr key={stat.id}>
+              <tr key={stat.id}>
                 <td>{stat.player_name}</td>
                 <td>{stat.goals}</td>
                 <td>{stat.shots}</td>
                 <td>{stat.shots_on_target}</td>
                 <td>{stat.passes}</td>
+                <td>{stat.assists}</td>
+                <td>{stat.tackles}</td>
                 {/* Add more table cells for other stats */}
-                </tr>
+              </tr>
             ))}
-            </tbody>
+          </tbody>
         </table>
-        </div>
+      </div>
     </PageContent>
-    
   );
 };
 
--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -1,30 +1,43 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { Container, Typography, Alert, Box, IconButton, InputAdornment, TextField, Link } from '@mui/material'; // Tambahkan Link dari MUI
-import { Visibility, VisibilityOff } from '@mui/icons-material';
-import { Navigate } from 'react-router-dom';
-import { Link as RouterLink } from 'react-router-dom'; // Import Link as RouterLink
-import './LoginPage.css'; // Import the CSS file for styling
+import React, { useState } from "react";
+import axios from "axios";
+import {
+  Container,
+  Typography,
+  Alert,
+  Box,
+  IconButton,
+  InputAdornment,
+  TextField,
+  Link,
+} from "@mui/material"; // Tambahkan Link dari MUI
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { Navigate } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom"; // Import Link as RouterLink
+import "./LoginPage.css"; // Import the CSS file for styling
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
-  const [error, setError] = useState('');
-  const [successMessage, setSuccessMessage] = useState('');
+  const [error, setError] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
   const [redirect, setRedirect] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const response = await axios.post('http://localhost:5000/api/login', { username, password });
+      const response = await axios.post("http://localhost:5000/api/login", {
+        username,
+        password,
+      });
+      // localStorage.setItem("hasLoggedIn", true);
       if (response.status === 200) {
-        setSuccessMessage('Login successful! Redirecting...');
+        setSuccessMessage("Login successful! Redirecting...");
         setTimeout(() => setRedirect(true), 2000);
       }
     } catch (error) {
-      setError('Invalid username or password');
+      setError("Invalid username or password");
     }
   };
 
@@ -41,7 +54,10 @@ const Login = () => {
       <Typography variant="h3" component="h2" gutterBottom>
         ScoreStatsFC
       </Typography>
-      <Container maxWidth="sm" className="login-container  border-2 border-black bg-[#EDF1D6]">
+      <Container
+        maxWidth="sm"
+        className="login-container  border-2 border-black bg-[#EDF1D6]"
+      >
         <Box my={4}>
           <Typography variant="h4" component="h1" gutterBottom>
             Login
@@ -64,7 +80,7 @@ const Login = () => {
               <TextField
                 variant="outlined"
                 fullWidth
-                type={showPassword ? 'text' : 'password'}
+                type={showPassword ? "text" : "password"}
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 InputProps={{
@@ -78,11 +94,23 @@ const Login = () => {
                 }}
               />
             </div>
-            <button className="submit-button" type="submit" style={{ backgroundColor: '#609966' }}>
+            <button
+              className="submit-button"
+              type="submit"
+              style={{ backgroundColor: "#609966" }}
+            >
               Login
             </button>
-            <Typography variant="body2" color="textSecondary" align="center" style={{ marginTop: '10px' }}>
-              Belum punya akun? <Link component={RouterLink} to="/signup">Daftar di sini</Link>
+            <Typography
+              variant="body2"
+              color="textSecondary"
+              align="center"
+              style={{ marginTop: "10px" }}
+            >
+              Belum punya akun?{" "}
+              <Link component={RouterLink} to="/signup">
+                Daftar di sini
+              </Link>
             </Typography>
           </form>
         </Box>
