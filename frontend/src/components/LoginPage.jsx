eb7202ac2d96afa2b10ed3c630a07e206f42145b
ReikiPD <reikiputradarmawan@gmail.com>
2024-06-08 21:37:53 +0700

    Push Reiki

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -37,11 +37,11 @@ const Login = () => {
   }
 
   return (
-    <div className="login-page">
+    <div className="login-page ">
       <Typography variant="h3" component="h2" gutterBottom>
         ScoreStatsFC
       </Typography>
-      <Container maxWidth="sm" className="login-container">
+      <Container maxWidth="sm" className="login-container  border-2 border-black bg-[#EDF1D6]">
         <Box my={4}>
           <Typography variant="h4" component="h1" gutterBottom>
             Login
